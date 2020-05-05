d69a536b32eb68f6d679b81aec3a858f50a7f220
Akira Kawamata <autumn@piedey.co.jp>
2020-05-05 19:35:09 +0900

    Play audio traial

--- softcmtif/softcmtif/Program.cs
+++ softcmtif/softcmtif/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace softcmtif
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if( args.Length != 1)
+            {
+                Console.Error.WriteLine("Soft CMT Interface by autumn");
+                Console.Error.WriteLine("usage: softcmtif INPUT_FILE_NAME");
+                return;
+            }
+
+            var audioStream = new AudioFileReader(args[0]);
+            audioStream.Position = 0;
+            var outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioStream);
+            outputDevice.Play();
+            await Task.Delay(10000);
+
+            Console.WriteLine("Done");
         }
     }
 }
