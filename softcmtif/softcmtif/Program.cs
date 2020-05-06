fcb18958d2fbfa4ef9c912828a90e7a1e157db78
Akira Kawamata <autumn@piedey.co.jp>
2020-05-06 11:32:59 +0900

    channel UI

--- softcmtif/softcmtif/Program.cs
+++ softcmtif/softcmtif/Program.cs
@@ -6,20 +6,42 @@ using NAudio.Wave;
 
 namespace softcmtif
 {
+    enum ChannelType
+    {
+        Null,
+        Right,
+        Left,
+        Average
+    }
+
+    class MyWaveReader
+    {
+
+
+        public MyWaveReader()
+        {
+
+        }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
+            ChannelType channelType = ChannelType.Null;
             if (args.Length == 0)
             {
                 Console.Error.WriteLine("Soft CMT Interface by autumn");
-                Console.Error.WriteLine("usage: softcmtif INPUT_FILE_NAME [--verbose]");
+                Console.Error.WriteLine("usage: softcmtif INPUT_FILE_NAME [--verbose] [--right|--left|--average]");
                 return;
             }
             bool bVerbose = false;
             foreach (var item in args.Skip(1))
             {
                 if (item == "--verbose") bVerbose = true;
+                else if (item == "--right") channelType = ChannelType.Right;
+                else if (item == "--left") channelType = ChannelType.Left;
+                else if (item == "--average") channelType = ChannelType.Average;
                 else
                 {
                     Console.WriteLine($"Unknwon option {item}");
@@ -27,30 +49,53 @@ namespace softcmtif
                 }
             }
             if (bVerbose) Console.WriteLine($"File Length: {new FileInfo(args[0]).Length}");
-            var audioStream = new AudioFileReader(args[0]);
-            audioStream.Position = 0;
-            if (bVerbose)
+            using (var audioStream = new AudioFileReader(args[0]))
             {
-                Console.WriteLine($"Audio Stream Length: {audioStream.Length}");
-                Console.WriteLine($"BlockAlign: {audioStream.BlockAlign}");
-                Console.WriteLine($"Channels: {audioStream.WaveFormat.Channels}");
-                Console.WriteLine($"BitsPerSample: {audioStream.WaveFormat.BitsPerSample}");
-                Console.WriteLine($"Encoding: {audioStream.WaveFormat.Encoding}");
-                Console.WriteLine($"ExtraSize: {audioStream.WaveFormat.ExtraSize}");
-                Console.WriteLine($"SampleRate: {audioStream.WaveFormat.SampleRate}");
+                audioStream.Position = 0;
+                if (bVerbose)
+                {
+                    Console.WriteLine($"Audio Stream Length: {audioStream.Length}");
+                    Console.WriteLine($"BlockAlign: {audioStream.BlockAlign}");
+                    Console.WriteLine($"Channels: {audioStream.WaveFormat.Channels}");
+                    Console.WriteLine($"BitsPerSample: {audioStream.WaveFormat.BitsPerSample}");
+                    Console.WriteLine($"Encoding: {audioStream.WaveFormat.Encoding}");
+                    Console.WriteLine($"ExtraSize: {audioStream.WaveFormat.ExtraSize}");
+                    Console.WriteLine($"SampleRate: {audioStream.WaveFormat.SampleRate}");
+                }
+                if (audioStream.WaveFormat.Channels == 1)
+                {
+                    if (channelType != ChannelType.Null)
+                    {
+                        Console.WriteLine("Mono file not support --right|--left|--average");
+                        return;
+                    }
+                }
+                else if (audioStream.WaveFormat.Channels == 2)
+                {
+                    if (channelType == ChannelType.Null) channelType = ChannelType.Average;
+                }
+                else
+                {
+                    Console.WriteLine($"Not supported channels {audioStream.WaveFormat.Channels}");
+                    return;
+                }
+                if (bVerbose) Console.WriteLine($"Channel selected: ChannelType:{channelType}");
+
+
+
+
+                //float[] samples = new float[audioStream.Length / audioStream.BlockAlign * audioStream.WaveFormat.Channels];
+                //audioStream.Read(samples, 0, samples.Length);
+
+
+
+                    // playback
+                    //var outputDevice = new WaveOutEvent();
+                    //outputDevice.Init(audioStream);
+                    //outputDevice.Play();
+                    //await Task.Delay(10000);
+
             }
-
-            //float[] samples = new float[audioStream.Length / audioStream.BlockAlign * audioStream.WaveFormat.Channels];
-            //audioStream.Read(samples, 0, samples.Length);
-
-
-
-            // playback
-            //var outputDevice = new WaveOutEvent();
-            //outputDevice.Init(audioStream);
-            //outputDevice.Play();
-            //await Task.Delay(10000);
-
             Console.WriteLine("Done");
         }
     }
